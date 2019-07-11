7ed095dcf3a085d6654c41c6e0d9c4304a888b0e
Cesar "Tito" Lopez <cesar.lopez.rodriguez@gmail.com>
2019-07-11 15:29:26 -0400

    homework- Node Express-handlebars

--- config/connection.js
+++ config/connection.js
@@ -3,8 +3,8 @@ var mysql = require("mysql");
 
 var connection;
 
-if (process.env.jawSDB_URL) {
-  connection = mysql.createConnection(process.env.jawSDB_URL)
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
   connection = mysql.createConnection({
   host: "localhost",
