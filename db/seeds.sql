fe60db043edcdbd2c4705802625a05dfe0129ffe
Cesar "Tito" Lopez <cesar.lopez.rodriguez@gmail.com>
2019-07-11 15:19:15 -0400

    homework- Node Express-handlebars

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('hawaian');
+INSERT INTO burger (name) VALUES ('tacoload');
+INSERT INTO burger (name, devour) VALUES ('jalapenohot', true);
+INSERT INTO burger(name, devour) VALUES ('criolle', true);
+INSERT INTO burger(name, devour) VALUES ('peruvian', true);
+INSERT INTO burger(name) VALUES ('wally');
