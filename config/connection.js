57e21c08bd3481cebd6cdbcfaaff18b86f67cddb
Cesar "Tito" Lopez <cesar.lopez.rodriguez@gmail.com>
2019-07-11 15:22:50 -0400

    homework- Node Express-handlebars

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,12 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+var connection;
 
 if (process.env.jawSDB_URL) {
   connection = mysql.createConnection(process.env.jawSDB_URL)
 }
-var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
