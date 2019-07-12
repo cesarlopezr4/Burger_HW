a4d5cff7cc9c32a20e199b5e1f92732c2450b42b
Cesar "Tito" Lopez <cesar.lopez.rodriguez@gmail.com>
2019-07-12 14:40:21 -0400

    connection changes

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection;
-// if (process.env.JAWSDB_URL) {
-  // connection =  mysql://vhvif13oq2au69ah:uvd7td369houpdaq@tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u903m14qs707n7je({
-    // connection = mysql.createConnection(process.env.JAWSDB_URL) ({
-//  connection = mysql.//vhvif13oq2au69ah:uvd7td369houpdaq@tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/u903m14qs707n7je) ({
-// }
-//   connection = mysql.createConnection({
-  var connection = mysql.createConnection(process.env.JAWSDB_URL) ({
-  host: "tkck4yllxdrw0bhi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+  connection = mysql.createConnection({
+  host: "localhost",
   port: 3306,
-  user: "vhvif13oq2au69ah",
-  password: "uvd7td369houpdaq",
-  database: "u903m14qs707n7je"
-  // connection = mysql.createConnection({
-  //    host: "localhost",
-  //    port: 3306,
-  //   user: "root",
-  //   password: "root",
-  //   database: "burger_db"
+  user: "root",
+  password: "root",
+  database: "burger_db"
 });
 
 connection.connect(function(err) {
